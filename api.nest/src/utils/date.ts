93087511ab61cc327bbb162f6e7cca4fdba93c2a
alxpgv <alex@ipcoder.ru>
2023-01-21 14:00:16 +0300

    format date

--- api.nest/src/utils/date.ts
+++ api.nest/src/utils/date.ts
@@ -1,8 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dayjs = require('dayjs');
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/ru';
 
 export const formatISODate = (value: string) => {
-  return dayjs(value).format('D.MM.YY');
+  return dayjs(value).format('YYYY.MM.DD');
 };
 
 export const formatISOTime = (value: string) => {
