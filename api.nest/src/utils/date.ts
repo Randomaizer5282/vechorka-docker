022a2dc78eb35e1599af71ddbba8ce47e248006b
alxpgv <alex@ipcoder.ru>
2023-03-14 15:16:48 +0300

    fix date format

--- api.nest/src/utils/date.ts
+++ api.nest/src/utils/date.ts
@@ -2,7 +2,7 @@ import * as dayjs from 'dayjs';
 import 'dayjs/locale/ru';
 
 export const formatISODate = (value: string) => {
-  return dayjs(value).format('YYYY.MM.DD');
+  return dayjs(value).format('YYYY-MM-DD');
 };
 
 export const formatISOTime = (value: string) => {
