1c992e9475751fadca7475a2d9912bde9ea29265
alxpgv <alex@ipcoder.ru>
2023-05-16 04:55:35 +0300

    add cache to settings/common

--- api.nest/src/modules/settings/settings.controller.ts
+++ api.nest/src/modules/settings/settings.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { SettingsService } from './settings.service';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('settings')
 export class SettingsController {
   constructor(private readonly settingsService: SettingsService) {}
 
+  @UseInterceptors(CacheInterceptor)
   @Get('common')
   getCommon() {
     return this.settingsService.getCommon();
--- api.nest/src/modules/settings/settings.module.ts
+++ api.nest/src/modules/settings/settings.module.ts
@@ -3,9 +3,17 @@ import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
 import { PostModule } from '../post/post.module';
 import { TaxonomyModule } from '../taxonomy/taxonomy.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [PostModule, TaxonomyModule],
+  imports: [
+    PostModule,
+    TaxonomyModule,
+    CacheModule.register({
+      ttl: 60000, // ml seconds
+      // max: 10, // maximum number of items in cache
+    }),
+  ],
   providers: [SettingsService],
   controllers: [SettingsController],
   exports: [SettingsService],
